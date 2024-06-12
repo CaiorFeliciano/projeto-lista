bb9e8ab407964f376cf75bb9b2764f5fc976beb9
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-06-11 21:26:06 -0300

    feat: interface, getData()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,24 @@ import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { ArrowDown, ArrowLeft, ArrowRight, Search } from "lucide-react";
-import { use, useState } from "react";
-import data from "./data.json";
+import { use, useEffect, useState } from "react";
 
-const columns: ColumnDef<(typeof data)[0]>[] = [
+//definindo a interface dos dados
+interface Aluno {
+  id_aluno: number;
+  nome_aluno: string;
+  email_aluno: string;
+  id_plano: number;
+  vencimento: string;
+  ultimo_acesso: string;
+}
+
+const columns: ColumnDef<Aluno>[] = [
   { accessorKey: "id_aluno", header: "ID" },
   { accessorKey: "nome_aluno", header: "Nome" },
   { accessorKey: "email_aluno", header: "E-Mail" },
@@ -20,23 +31,49 @@ const columns: ColumnDef<(typeof data)[0]>[] = [
   { accessorKey: "ultimo_acesso", header: "Último Acesso" },
 ];
 
-async function getData() {
+async function getData(): Promise<Aluno[]> {
   const res = await fetch("http://localhost:8080/alunos");
+  if (!res.ok) {
+    throw new Error("Falha em buscar os dados");
+  }
   return res.json();
 }
 
 export default function Home() {
   const [globalFilter, setGlobalFilter] = useState("");
-  const alunos = use(getData());
+  const [alunos, setALunos] = useState<Aluno[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getData();
+        console.log(data);
+        setALunos(data);
+      } catch (error) {
+        console.error("Falha ao conseguir os dados", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   const table = useReactTable({
     columns,
-    alunos,
+    data: alunos,
     state: {
       globalFilter,
     },
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <main className="my-6 mx-2">
@@ -53,23 +90,27 @@ export default function Home() {
       <table className="table">
         <thead>
           <tr>
-            {table.getHeaderGroups()[0].headers.map((header) => (
+            {table.getFlatHeaders().map((header) => (
               <th key={header.id}>
                 <div className="flex">
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
                   )}{" "}
-                  <ArrowDown size={16} className="cursor-pointer" />
+                  <ArrowDown
+                    size={16}
+                    className="cursor-pointer"
+                    onClick={() => header.column.toggleSorting(false)}
+                  />
                 </div>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {table.getRowModel().alunos.map((aluno) => (
-            <tr key={aluno.id}>
-              {aluno.getVisibleCells().map((cell) => (
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
                 <td>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
@@ -80,9 +121,19 @@ export default function Home() {
       </table>
 
       <div className="flex justify-center gap-12">
-        <ArrowLeft className="cursor-pointer" />
-        <span>1</span>
-        <ArrowRight className="cursor-pointer" />
+        {table.getCanPreviousPage() && (
+          <ArrowLeft
+            className="cursor-pointer"
+            onClick={() => table.previousPage()}
+          />
+        )}
+        <span>{table.getState().pagination.pageIndex + 1}</span>
+        {table.getCanNextPage() && (
+          <ArrowRight
+            className="cursor-pointer"
+            onClick={() => table.nextPage()}
+          />
+        )}
       </div>
     </main>
   );
